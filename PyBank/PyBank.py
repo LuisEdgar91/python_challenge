826f38cededf71e23165f31098905ecf677e8d90
LuisEdgar91 <luisedgar2017@outlook.com>
2024-03-06 14:57:10 -0600

    Adding work files

---
+++ PyBank/PyBank.py
@@ -0,0 +1,74 @@
+# Import os and csv modules
+import os
+import csv
+
+#Define file paths
+csvpath = os.path.join("budget_data.csv")
+output_file_path = 'financial_analysis.txt'
+
+#Define initial variables
+total_months = 0
+net_total = 0
+previous_profit_loss = 0
+changes = []
+greatest_increase = ["", 0]
+greatest_decrease = ["", 0]
+
+#Open csv file
+with open(csvpath) as csvfile:
+
+    #Create csv reader object
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    #Skip headers
+    csv_header = next(csvreader)
+
+    # Read each row of data after the header
+    for row in csvreader:
+        
+        #Sum months
+        total_months += 1
+        
+        #Sum profit/loss value to the total
+        net_total += int(row[1])
+
+        #Calculate change in profit/loss
+        current_profit_loss = int(row[1])
+        if total_months > 1:
+            change = current_profit_loss - previous_profit_loss
+            changes.append(change)
+
+            #Check greatest increase and decrease
+            if change > greatest_increase[1]:
+                greatest_increase = [row[0], change]
+            if change < greatest_decrease[1]:
+                greatest_decrease = [row[0], change]
+        
+        previous_profit_loss = current_profit_loss
+    
+    total_changes = sum(changes)
+    
+    #Calculate the average change
+    average_change = total_changes / (total_months - 1)
+    
+    #Print results
+    print ("Financial Analysis")
+    print ('-' * 40)
+    print ("Total Months: " + str(total_months))
+    print("Total: $" + str(net_total))
+    print("Average Change: $" + str(round(average_change,2)))
+    print(f"Greatest Increase in Profits: {greatest_increase[0]} (${greatest_increase[1]})")
+    print(f"Greatest Decrease in Profits: {greatest_decrease[0]} (${greatest_decrease[1]})")    
+
+#Prepare the results to appear in text file
+result_text = f"Financial Analysis\n"
+result_text += f"------------------\n"
+result_text += f"Total Months: {total_months}\n"
+result_text += f"Total: ${net_total}\n"
+result_text += f"Average Change: ${average_change:.2f}\n"
+result_text += f"Greatest Increase in Profits: {greatest_increase[0]} (${greatest_increase[1]})\n"
+result_text += f"Greatest Decrease in Profits: {greatest_decrease[0]} (${greatest_decrease[1]})\n"
+
+# Write results into a txt file
+with open(output_file_path, 'w') as file:
+    file.write(result_text)
---
+++ Pypoll/PyPoll.py
@@ -0,0 +1,71 @@
+# Import os and csv modules
+import csv
+import os
+
+#Define file paths
+csvpath = os.path.join('Resources', 'election_data.csv')
+output_file_path = 'output_elections_result.txt'
+
+#Define initial variables
+total_votes = 0
+candidate_votes = {}
+candidates = []
+
+#Open csv file
+with open(csvpath) as csvfile:
+    
+    #Create csv reader object
+    csvreader = csv.reader(csvfile, delimiter=',')
+    
+    #Skip headers
+    next(csvreader)
+    
+    # Read each row of data after the header
+    for row in csvreader:
+        
+        # Increment the total number of votes
+        total_votes += 1
+        
+        # Get the candidate name from the current row
+        candidate = row[2]
+        
+        # Check if the candidate is not already in the dictionary
+        if candidate not in candidate_votes:
+            # Initialize the vote count for the candidate
+            candidate_votes[candidate] = 0
+            # Add the candidate to the list of candidates
+            candidates.append(candidate)
+        # Increment the vote count for the candidate
+        candidate_votes[candidate] += 1
+
+# Calculate the percentage of votes each candidate won
+percentages = {candidate: (votes / total_votes) * 100 for candidate, votes in candidate_votes.items()}
+
+# Find the winner of the election based on popular vote
+winner = max(candidate_votes, key=candidate_votes.get)
+
+# Print the analysis results
+print("Election Results")
+print('-' * 40)
+print(f"Total Votes: {total_votes}")
+print('-' * 40)
+for candidate in candidates:
+    print(f"{candidate}: {percentages[candidate]:.3f}% ({candidate_votes[candidate]})")
+print('-' * 40)
+print(f"Winner: {winner}")
+print('-' * 40)
+
+#Prepare the results to appear in text file
+result_text = f"Election Results\n"
+result_text += f"------------------\n"
+result_text += f"Total Votes: {total_votes}\n"
+result_text += f"------------------\n"
+for candidate in candidates:
+    result_text += f"{candidate}: {percentages[candidate]:.3f}% ({candidate_votes[candidate]})\n"
+result_text += f"------------------\n"
+result_text += f"Winner: {winner}\n"
+result_text += f"------------------\n"
+
+# Write results into a txt file
+with open(output_file_path, 'w') as file:
+    file.write(result_text)
